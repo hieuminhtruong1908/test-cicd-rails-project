1d44c8faefc8663602fd350d5579ace6f389b97d
NewIT - HieuTM <hieutm@newit.jp.co>
2024-05-29 14:31:30 +0700

    Add workflow - deploy

--- docker/wait-for-services.sh
+++ docker/wait-for-services.sh
@@ -7,11 +7,3 @@ do
   sleep 1
 done
 echo "PostgreSQL is up and running"
-
-# Wait for OpenSearch
-until nc -z -v -w30 "$OPENSEARCH_HOST" 9200
-do
-  echo 'Waiting for OpenSearch...'
-  sleep 1
-done
-echo "OpenSearch is up and running"
